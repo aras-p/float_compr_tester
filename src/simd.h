b89d880df8b43777329a2cb2ef16d322defaa1af
Aras Pranckevicius <aras@nesnausk.org>
2023-02-19 20:54:43 +0200

    opt: simd.h NEON fixes

--- src/simd.h
+++ src/simd.h
@@ -44,23 +44,23 @@ inline Bytes16 SimdPrefixSum(Bytes16 x)
     return x;
 }
 
-#elif CPU_ARCH_X64
+#elif CPU_ARCH_ARM64
 typedef uint8x16_t Bytes16;
 inline Bytes16 SimdZero() { return vdupq_n_u8(0); }
 inline Bytes16 SimdSet1(uint8_t v) { return vdupq_n_u8(v); }
-inline Bytes16 SimdLoad(const void* ptr) { return vld1q_u8(ptr); }
-inline Bytes16 SimdLoadA(const void* ptr) { return vld1q_u8(ptr); }
-inline void SimdStore(void* ptr, Bytes16 x) { vst1q_u8(ptr, x); }
-inline void SimdStoreA(void* ptr, Bytes16 x) { vst1q_u8(ptr, x); }
+inline Bytes16 SimdLoad(const void* ptr) { return vld1q_u8((const uint8_t*)ptr); }
+inline Bytes16 SimdLoadA(const void* ptr) { return vld1q_u8((const uint8_t*)ptr); }
+inline void SimdStore(void* ptr, Bytes16 x) { vst1q_u8((uint8_t*)ptr, x); }
+inline void SimdStoreA(void* ptr, Bytes16 x) { vst1q_u8((uint8_t*)ptr, x); }
 
 template<int lane> inline uint8_t SimdGetLane(Bytes16 x) { return vgetq_lane_u8(x, lane); }
 template<int lane> inline Bytes16 SimdSetLane(Bytes16 x, uint8_t v) { return vsetq_lane_u8(v, x, lane); }
 template<int index> inline Bytes16 SimdConcat(Bytes16 hi, Bytes16 lo) { return vextq_u8(lo, hi, index); }
 
-inline Bytes16 operator+(Bytes16 a, Bytes16 b) { return vaddq_u8(a, b); }
-inline Bytes16 operator-(Bytes16 a, Bytes16 b) { return vsubq_u8(a, b); }
-
 inline Bytes16 SimdShuffle(Bytes16 x, Bytes16 table) { return vqtbl1q_u8(x, table); }
+inline Bytes16 SimdInterleaveL(Bytes16 a, Bytes16 b) { return vzip1q_s8(a, b); }
+inline Bytes16 SimdInterleaveR(Bytes16 a, Bytes16 b) { return vzip2q_s8(a, b); }
+
 
 inline Bytes16 SimdPrefixSum(Bytes16 x)
 {
