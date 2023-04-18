755f36e00d0568c8dd8fef71c3a8f4b16e0f546f
Aras Pranckevicius <aras@nesnausk.org>
2023-04-18 10:03:36 +0300

    LZSSE8 on arm64: use sse2neon.h

--- libs/lzsse/lzsse8/lzsse8_platform.h
+++ libs/lzsse/lzsse8/lzsse8_platform.h
@@ -52,11 +52,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  GCC
 */
 
+#if defined(__aarch64__)
+#include "../../sse2neon/sse2neon.h"
+#else
 /*
  Note: including just <smmintrin.h> would be sufficient, but including x86intrin is a better match to intrin.h on Visual Studio as
  both include all intrinsics for the enabled processor, rather than just SSE4.1.
 */
 #include <x86intrin.h>
+#endif
 /* _BitScanForward is Visual Studio specific. */
 #define _BitScanForward(x, m) *(x) = __builtin_ctz(m)
 
